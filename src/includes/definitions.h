983bf35137e660717ec96e12256295e25bbc7760
Lorenzo Rocca <lorenzo.rocca.work@gmail.com>
2024-12-07 23:33:09 +0100

    Diminuita la dimensione del terminale

--- src/includes/definitions.h
+++ src/includes/definitions.h
@@ -8,6 +8,6 @@
 #define VERSION                             "1.0.0/DEV"
 
 // Impostazioni dei form
-#define FORM_WIDTH                          100
+#define FORM_WIDTH                          80
 
 #pragma endregion
